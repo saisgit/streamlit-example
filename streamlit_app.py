efb583bb2774c006683b8f55aec42f89277a6f74
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-17 23:24:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,30 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 st.write("hi sai")
+
+class NSE():
+  pre_market_categories = ['NIFTY 50','Nifty Bank','Emerge','Securities in F&O','Others','All']
+  equity_market_categories = []
+  holidays_categories = ["Clearing","Trading"]
+
+  def __init__(self):
+    self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
+    self.session = requests.Session()
+    self.session.get("http://nseindia.com",headers=self.headers)
+
+  def equity_market_data(self,category,symbol_list=False):
+    category = category.upper().replace(' ','%20').replace('&','%26')
+    data = self.session.get(f"https://www.nseindia.com/api/equity-stockIndices?index={category}",headers=self.headers).json()["data"]
+    df = pd.DataFrame(data)
+    df = df.drop(["meta"],axis=1)
+    df = df.set_index("symbol",drop=True)
+    if symbol_list:
+      return list(df.index)
+    else:
+      return df
+
+nse = NSE()
+high = nse.equity_market_data("Securities in F&O")[['open','dayHigh','dayLow','lastPrice','totalTradedVolume','previousClose']].reset_index()
+high = high.rename(columns={"totalTradedVolume": "volume",'lastPrice':'Close'})
+high = round(high,2)
+st.write(high)
