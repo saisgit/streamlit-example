fe016aa9f60ec5755e17c0846798bd0deee12006
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-18 22:20:00 +0530

    Update analysis.py

--- pages/analysis.py
+++ pages/analysis.py
@@ -97,16 +97,193 @@ def volshock(todayshock):
     if todayshock > 1.4:
         return 'background-color: green'
 
+
+
+def myanalysis(current_day_dmy,previous_day_dmy,dby_day_dmy):
+    nse = NSE()
+    fno = nse.equity_market_data('Securities in F&O',symbol_list=True)
+    n50 = nse.equity_market_data('NIFTY 50',symbol_list=True)
+    n50.remove("NIFTY 50")
+    df1 = nse.equity_market_data("Securities in F&O")[['open','dayHigh','dayLow','lastPrice','totalTradedVolume','previousClose']].reset_index()
+    df1 = df1.rename(columns={"totalTradedVolume": "volume"})
+    df1 = round(df1,2)
+    all = fno
+    ydy = nse.getbhavcopy(previous_day_dmy)
+    ydy = ydy[ydy['SYMBOL'].isin(all)]
+    ydy.columns = ydy.columns.str.lower()
+    ydy = ydy.add_prefix('Yest')
+    dbdy = nse.getbhavcopy(dby_day_dmy)
+    dbdy = dbdy[dbdy['SYMBOL'].isin(all)]
+    dbdy.columns = dbdy.columns.str.lower()
+    dbdy = dbdy.add_prefix('dby')
+    full = df1.set_index('symbol').join(ydy.set_index('Yestsymbol'), on='symbol')
+    full.reset_index(inplace=True)
+    full = full.set_index('symbol').join(dbdy.set_index('dbysymbol'), on='symbol')
+    full.reset_index(inplace=True)
+    volt = get_vol(previous_day_dmy)
+    full = full.set_index('symbol').join(volt.set_index('symbol'), on='symbol')
+    full.reset_index(inplace=True)
+    full['prevdata'] = np.where((full.dayHigh.astype(float) >  full.Yesthigh_price.astype(float)), "brkPrevHigh",np.where((full.dayLow.astype(float) <  full.Yestlow_price.astype(float)), "brkPrevlow","")) 
+    full['pp'] = (full.Yesthigh_price.astype(float) + full.Yestlow_price.astype(float) + full.Yestclose_price.astype(float))/3
+    full['yest_pp'] = (full.dbyhigh_price.astype(float) + full.dbylow_price.astype(float) + full.Yestprev_close.astype(float))/3
+    full['bc'] = round((full.Yesthigh_price.astype(float) + full.Yestlow_price.astype(float))/2,2)
+    full['yest_bc'] = round((full.dbyhigh_price.astype(float) + full.dbylow_price.astype(float))/2,2)
+    full['tc'] = round((full.pp.astype(float) - full.bc.astype(float))+full.pp.astype(float),2)
+    full['yest_tc'] = round((full.yest_pp.astype(float) - full.yest_bc.astype(float))+full.yest_pp.astype(float),2)
+    full['r1'] = round((2 * full.pp.astype(float)) - full.Yestlow_price.astype(float),2)
+    full['s1'] = round((2 * full.pp.astype(float)) - full.Yesthigh_price.astype(float),2)
+    full['r2'] = (full.pp.astype(float)) + (full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))
+    full['yest_r2'] = (full.yest_pp.astype(float)) + (full.dbyhigh_price.astype(float) - full.dbylow_price.astype(float))
+    full['s2'] = (full.pp.astype(float)) - (full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))
+    full['camR4'] = (full.Yestclose_price.astype(float)) + (((full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))/2)*float(1.1))
+    full['camS4'] = (full.Yestclose_price.astype(float)) - (((full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))/2)*float(1.1))
+    full['camR3'] = (full.previousClose.astype(float)) + (((full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))/4)*float(1.1))
+    full['camS3'] = (full.previousClose.astype(float)) - (((full.Yesthigh_price.astype(float) - full.Yestlow_price.astype(float))/4)*float(1.1))
+    #S3 = C - [1.1 * (H - L) / 4]
+    full['YcamR3'] = (full.Yestprev_close.astype(float)) + (((full.dbyhigh_price.astype(float) - full.dbylow_price.astype(float))/4)*float(1.1))
+    full['YcamS3'] = (full.Yestprev_close.astype(float)) - (((full.dbyhigh_price.astype(float) - full.dbylow_price.astype(float))/4)*float(1.1))
+    full['rdist'] = abs(round(((full.r2.astype(float)-full.r1.astype(float))/full.r2.astype(float))*100,2))
+    full['sdist'] = abs(round(((full.s1.astype(float)-full.s2.astype(float))/full.s1.astype(float))*100,2))
+    full['cpr'] = round((abs(full.tc.astype(float)-full.bc.astype(float))/full.bc.astype(float))*100,2)
+    full['yest_cpr'] = round((abs(full.yest_tc.astype(float)-full.yest_bc.astype(float))/full.yest_bc.astype(float))*100,2)
+    full['gaps'] = np.where(((full.open.astype(float) >= full.Yesthigh_price.astype(float)) & (full.open.astype(float) > full.Yestclose_price.astype(float).mul(1.002))), "GapUp",np.where((full.open.astype(float) <= full.Yestlow_price.astype(float)) & (full.open.astype(float) < full.Yestclose_price.astype(float).mul(0.998)), "GapDown",""))
+    full['cams'] = np.where(((full.camR3.astype(float) <= full.YcamR3.astype(float)) & (full.camS3.astype(float) >= full.YcamS3.astype(float))), "CM","")
+    full['candle'] = np.where(((full.Yesthigh_price.astype(float) <= full.dbyhigh_price.astype(float)) & (full.Yestlow_price.astype(float) >= full.dbylow_price.astype(float))), "inside",np.where((full.Yestlow_price.astype(float) <= full.dbylow_price.astype(float)) & (full.Yesthigh_price.astype(float) >= full.dbyhigh_price.astype(float)), "outside",""))
+    full['candle'] = full['candle'] + full['cpr'].astype(str)
+    full['N50'] = np.where(full['symbol'].isin(n50), "Y","")
+    fnolen = len(fno)
+    if fnolen < 4:
+        ips = 2
+    elif fnolen >16:
+        ips = 4
+    else:
+        ips = 1
+    allsplits = np.array_split(all, ips)
+    s = time.time()
+    tasks = map(lambda x:(allsplits[x],previous_day_dmy),range(0,ips))
+    with Pool(ips) as executor:
+      results = executor.starmap(get5minMC,iterable=tasks)
+      executor.close()
+    bb_df = pd.concat(results)
+    bb_df = bb_df.set_index('symbol').join(full.set_index('symbol'), on='symbol') #df1
+    bb_df.reset_index(inplace=True)
+    bb_df['cls_5m_r2'] = np.where((bb_df.Close.astype(float) <= bb_df['yest_r2'].astype(float)), "<5m_R2","")
+    bbs = fno
+    if bbs:
+        bblen = len(bbs)
+        if bblen < 4:
+            ipsplits = 1
+        elif bblen >16:
+            ipsplits = 4
+        else:
+            ipsplits = 2
+        ## GET 15min DATA
+        allsplits15 = np.array_split(bbs, ipsplits)
+        tasks1 = map(lambda x:(allsplits15[x],previous_day_dmy,'Y'),range(0,ipsplits))
+        with Pool(ipsplits) as executor:
+                results = executor.starmap(get15minMC,iterable=tasks1)
+        my15mDF = pd.concat(results)
+        allsplits1 = np.array_split(bbs, ipsplits)
+        tasks1 = map(lambda x:(allsplits1[x],previous_day_dmy),range(0,ipsplits))
+        with Pool(ipsplits) as executor:
+                results = executor.starmap(get1hourMC,iterable=tasks1)
+        stDF = pd.concat(results)
+        allsplits2 = np.array_split(bbs, ipsplits)
+        tasks2 = map(lambda x:(allsplits2[x],previous_day_dmy),range(0,ipsplits))
+        with Pool(ipsplits) as executor:
+                results = executor.starmap(get1dayMC,iterable=tasks2)
+        mDF = pd.concat(results)
+        ## GET HOURLY PIVOT DATA
+        allsplits3 = np.array_split(bbs, ipsplits)
+        tasks3 = map(lambda x:(allsplits3[x],previous_day_dmy),range(0,ipsplits)) # current_day_dmy change to previousday when running for before day
+        with Pool(ipsplits) as executor:
+                results = executor.starmap(hourPivots,iterable=tasks3)
+        pivotDF = pd.concat(results)
+        ## GET DAILY PIVOT DATA
+        allsplits4 = np.array_split(bbs, ipsplits)
+        tasks4 = map(lambda x:(allsplits4[x],current_day_dmy),range(0,ipsplits))
+        with Pool(ipsplits) as executor:
+                results = executor.starmap(getDayPivots,iterable=tasks4)
+        daypivotDF = pd.concat(results)
+        e = time.time()
+        st.write("Time Taken:{}".format(e - s))
+        bb_df1 = bb_df.set_index('symbol').join(stDF.set_index('symbol'), on='symbol')
+        bb_df1.reset_index(inplace=True)
+        bb_df2 = bb_df1.set_index('symbol').join(mDF.set_index('symbol'), on='symbol')
+        bb_df2.reset_index(inplace=True)
+        bb_df3 = bb_df2.set_index('symbol').join(pivotDF.set_index('symbol'), on='symbol')
+        bb_df3.reset_index(inplace=True)
+        bb_df4 = bb_df3.set_index('symbol').join(daypivotDF.set_index('symbol'), on='symbol')
+        bb_df4.reset_index(inplace=True)
+        bb_df = bb_df4.set_index('symbol').join(my15mDF.set_index('symbol'), on='symbol')
+        bb_df.reset_index(inplace=True)
+        ## GET HOURLY PIVOTS DATA
+        conditions = [
+            (bb_df.Open.astype(float) <= bb_df.pp_hour.astype(float)) & (bb_df.Close.astype(float) > bb_df.pp_hour.astype(float)),
+            (bb_df.Open.astype(float) <= bb_df.r1_hour.astype(float)) & (bb_df.Close.astype(float) > bb_df.r1_hour.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.pp_hour.astype(float)) & (bb_df.Close.astype(float) < bb_df.r1_hour.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.r1_hour.astype(float)) & (bb_df.Close.astype(float) < bb_df.r2_hour.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.r2_hour.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.pp_hour.astype(float)) & (bb_df.Close.astype(float) > bb_df.s1_hour.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.s1_hour.astype(float)) & (bb_df.Close.astype(float) > bb_df.s2_hour.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.s2_hour.astype(float))
+            ]
+        choices = ['crsPP','crsR1','pp-R1', 'R1-R2', '>R2','pp-S1','S1-S2','<S2']
+        bb_df['hourPvt'] = np.select(conditions, choices, default='')
+        ## GET DIALY PIVOT DATA
+        conditions = [
+            (bb_df.Open.astype(float) <= bb_df.pp_day.astype(float)) & (bb_df.Close.astype(float) > bb_df.pp_day.astype(float)),
+            (bb_df.Open.astype(float) <= bb_df.r1_day.astype(float)) & (bb_df.Close.astype(float) > bb_df.r1_day.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.pp_day.astype(float)) & (bb_df.Close.astype(float) < bb_df.r1_day.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.r1_day.astype(float)) & (bb_df.Close.astype(float) < bb_df.r2_day.astype(float)),
+            (bb_df.Close.astype(float) >= bb_df.r2_day.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.pp_day.astype(float)) & (bb_df.Close.astype(float) > bb_df.s1_day.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.s1_day.astype(float)) & (bb_df.Close.astype(float) > bb_df.s2_day.astype(float)),
+            (bb_df.Close.astype(float) <= bb_df.s2_day.astype(float))
+            ]
+        choices = ['crsPP','crsR1','pp-R1', 'R1-R2', '>R2','pp-S1','S1-S2','<S2']
+        bb_df['dayPvt'] = np.select(conditions, choices, default='')
+        ## GET SIGNAL
+        conditions = [
+            (bb_df.dayPvt.isin(["pp-R1"])) & (bb_df.hourPvt.isin(['crsPP','pp-R1','crsR1'])),
+            (bb_df.dayPvt.isin(["crsPP"])) & (bb_df.hourPvt.isin(['crsPP','pp-R1','crsR1'])),
+            (bb_df.dayPvt.isin(["crsPP","pp-R1"])) & (bb_df.hourPvt.isin(['R1-R2','pp-S1'])),
+            (bb_df.dayPvt.isin(["crsPP","pp-R1"])) & (bb_df.hourPvt.isin(['>R2'])),
+            (bb_df.dayPvt == "crsR1") & (bb_df.hourPvt.isin(['crsPP','pp-R1','crsR1'])),
+            (bb_df.dayPvt == "crsR1") & (bb_df.hourPvt.isin(['R1-R2','>R2'])),
+            (bb_df.dayPvt == "R1-R2") & (bb_df.hourPvt.isin(['crsPP','pp-R1','crsR1'])),
+            (bb_df.dayPvt == "R1-R2") & (bb_df.hourPvt.isin(['R1-R2'])),
+            (bb_df.hourPvt.isin(['crsPP']) ), #& (bb_df.dayvol.str.contains("g")
+            ]
+        choices = ['PBUY','BUY','BUY','B', 'BUY', 'B','BUY','B','BUY']
+        bb_df['go'] = np.select(conditions, choices, default='')
+        bb_df['bb_day_dist'] = round(((bb_df['Close'].astype(float)-bb_df['BBU_1day'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        #bb_df['go'] = np.where((bb_df.hourPvt == "crsR1") | (bb_df.hourBB == "crsBBU") | (bb_df.hourST == "crsA_hrST") | (bb_df.dayBB == "crsBBU_D") | (bb_df.dayST == "crsA_dST"), "PBUY",np.where((bb_df.hourPvt == "S1-S2"), "B",""))
+        
+        bb_df['1hrPP'] = round(((bb_df['Close'].astype(float)-bb_df['pp_hour'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['SMA_50_15m_d'] = round(((bb_df['Close'].astype(float)-bb_df['SMA_50_15m'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['SMA_20_1hr_d'] = round(((bb_df['Close'].astype(float)-bb_df['SMA_20_1hr'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['1hrR1'] = round(((bb_df['Close'].astype(float)-bb_df['r1_hour'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['1hrS1'] = round(((bb_df['Close'].astype(float)-bb_df['s1_hour'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['1hrR2'] = round(((bb_df['Close'].astype(float)-bb_df['r2_hour'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['1hrbb_U'] = round(((bb_df['Close'].astype(float)-bb_df['BBU_1hr'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['1hrbb_L'] = round(((bb_df['Close'].astype(float)-bb_df['BBL_1hr'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        bb_df['EMA_20_dist'] = round((abs(bb_df['Close'].astype(float)-bb_df['EMA_20'].astype(float))/bb_df['Close'].astype(float))*100,2)
+        
+        bb_df['pp_dist'] = np.where((bb_df['1hrPP'].astype(float) >=float(-0.5)) & (bb_df['1hrPP'].astype(float) <=float(0.5)), "P1",np.where((bb_df['1hrPP'].astype(float) >=float(-1)) & (bb_df['1hrPP'].astype(float) <=float(1)),"P2",np.where((bb_df['1hrPP'].astype(float) >=float(-1.5)) & (bb_df['1hrPP'].astype(float) <=float(1.5)),"P3","")))
+        bb_df['SMA_50_15m_dist'] = np.where((bb_df['SMA_50_15m_d'].astype(float) >=float(-0.5)) & (bb_df['SMA_50_15m_d'].astype(float) <=float(0.5)), "P1",np.where((bb_df['SMA_50_15m_d'].astype(float) >=float(-1)) & (bb_df['SMA_50_15m_d'].astype(float) <=float(1)),"P2",np.where((bb_df['SMA_50_15m_d'].astype(float) >=float(-1.5)) & (bb_df['SMA_50_15m_d'].astype(float) <=float(1.5)),"P3","")))
+        bb_df['SMA_20_1hr_dist'] = np.where((bb_df['SMA_20_1hr_d'].astype(float) >=float(-0.5)) & (bb_df['SMA_20_1hr_d'].astype(float) <=float(0.5)), "P1",np.where((bb_df['SMA_20_1hr_d'].astype(float) >=float(-1)) & (bb_df['SMA_20_1hr_d'].astype(float) <=float(1)),"P2",np.where((bb_df['SMA_20_1hr_d'].astype(float) >=float(-1.5)) & (bb_df['SMA_20_1hr_d'].astype(float) <=float(1.5)),"P3","")))
+        fulldf = bb_df.loc[:,['symbol','go','pp_dist','SMA_50_15m_dist','SMA_20_1hr_dist','rdist','sdist','bb5mdiff','bbands15m','todayshock','shock','dayvol','cls_5m_r2','hourPvt','dayPvt','bb_crs','N50','candle','ema50vwap','ev','pr_dist_hr','ps_dist_hr']]
+        fulldf = fulldf.drop_duplicates()
+        fulldf = fulldf.style.applymap(volshock, subset=['todayshock','shock'])
+        end = time.time()
+        print("Time Taken:{}".format(end - start))
+    return fulldf
+
 current_day_dmy = st.text_input('current_day_dmy', 'ddmmyyyy')
 previous_day_dmy = st.text_input('previous_day_dmy', 'ddmmyyyy')
 dby_day_dmy = st.text_input('dby_day_dmy', 'ddmmyyyy')
-
-def myanalysis(current_day_dmy,previous_day_dmy,dby_day_dmy):
-  nse = NSE()
-  fno = nse.equity_market_data('Securities in F&O',symbol_list=True)
-  n50 = nse.equity_market_data('NIFTY 50',symbol_list=True)
-  n50.remove("NIFTY 50")
-  df1 = nse.equity_market_data("Securities in F&O")[['open','dayHigh','dayLow','lastPrice','totalTradedVolume','previousClose']].reset_index()
-  df1 = df1.rename(columns={"totalTradedVolume": "volume"})
-  df1 = round(df1,2)
-  
+if st.button("Get analysis"):
+  if __name__ ==  '__main__':
+    df = myanalysis(current_day_dmy,previous_day_dmy,dby_day_dmy)
+    st.write(df)
