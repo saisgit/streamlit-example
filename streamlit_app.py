4fde5cfd197f0f1700447ffa665932e25a9e7f4a
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-18 15:26:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,16 @@ st.set_page_config(
 )
 st.title("Home")
 #st.sidebar.success("Select pages")
+st.set_page_config(layout="wide")
+padding_top = 0
+st.markdown(f"""
+    <style>
+        .reportview-container .main .block-container{{
+            padding-top: {padding_top}rem;
+        }}
+    </style>""",
+    unsafe_allow_html=True,
+)
 """
 # Welcome to Sai Stocks!
 """
