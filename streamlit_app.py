3dec5bc642310ccbddbd485fac86468c91e7aeb9
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-18 20:46:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,19 +77,19 @@ styles = [
   ]
 
 col1, col2 = st.columns(2)
-with col1:
-  st.header("sell")
-  data = conn.read(worksheet="Sheet2",usecols=list(range(7)),ttl="0").dropna(how="all")
-  df = pd.DataFrame(data).head(10)
-  df = df.reset_index(drop=True)
-  df2=df.style.set_properties(**{'text-align': 'left'}).set_table_styles(styles)
-  st.table(df2)
-with col2:
-  st.header("buy")
-  data = conn.read(worksheet="Sheet2",usecols=list(range(7)),ttl="0").dropna(how="all")
-  df = pd.DataFrame(data).head(10)
-  df2=df.style.set_properties(**{'text-align': 'left'}).set_table_styles(styles)
-  st.table(df2)
+# with col1:
+#   st.header("sell")
+#   data = conn.read(worksheet="Sheet2",usecols=list(range(7)),ttl="0").dropna(how="all")
+#   df = pd.DataFrame(data).head(10)
+#   df = df.reset_index(drop=True)
+#   df2=df.style.set_properties(**{'text-align': 'left'}).set_table_styles(styles)
+#   st.table(df2)
+# with col2:
+#   st.header("buy")
+#   data = conn.read(worksheet="Sheet2",usecols=list(range(7)),ttl="0").dropna(how="all")
+#   df = pd.DataFrame(data).head(10)
+#   df2=df.style.set_properties(**{'text-align': 'left'}).set_table_styles(styles)
+#   st.table(df2)
 #data = conn.read(worksheet="Sheet2",ttl="0")
 
 #if st.button("update"):
