52ccfd5ebebaf3e082046cf8ff74ced708aa0474
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-18 21:37:21 +0530

    Update myanalysis.py

--- myanalysis.py
+++ myanalysis.py
@@ -155,44 +155,44 @@ def get15minMC(fno,current_day_dmy,flag):
         mydf.reset_index(inplace=True)
     return mydf.loc[:, ['symbol','bbands15m','BBU_50_15m','BBL_50_15m','SMA_50_15m','vwap','EMA50_15m','ema50vwap','ev']]
 
-def get1hourMC_bkp(fno,current_day_dmy):
-    mydf = pd.DataFrame()
-    for stock in fno:
-        tday = datetime.strptime(current_day_dmy, '%d%m%Y').date() + timedelta(days=1)
-        nse = Moneycontrol()
-        df = nse.hist_data(stock,current_day_dmy,'60','3000')
-        pd.set_option('display.max_columns', None)
-        df['symbol'] = stock
-        df.reset_index(inplace=True)
-        df.set_index(pd.DatetimeIndex(df["Datetime"]).tz_localize(None), inplace=True)
-        #df['d_adx'] = ta.adx(df['High'], df['Low'], df['Close'],length=14)['ADX_14']
-        df.ta.ema(length=20, append=True)
-        df.ta.sma(length=20, append=True)
-        df.ta.bbands(close='Close', length=50, std=2, append=True) #BBL_50_2  BBM_50_2  BBU_50_2
-        df.ta.supertrend(period=7, multiplier=3, append=True)
-        df = df.rename(columns={"SUPERT_7_3.0":"hour_st"})
-        df['bbdiff1hr'] = round(((df['BBU_50_2.0'].astype(float)-df['BBL_50_2.0'].astype(float))/df['BBU_50_2.0'].astype(float))*100,2)
-        df['BBU_1hr'] = df['BBU_50_2.0'].astype(float)
-        df['BBL_1hr'] = df['BBL_50_2.0'].astype(float)
-        df['SMA_50'] = df['BBM_50_2.0'].astype(float)
-        df['SMA_20_1hr'] = df['SMA_20'].astype(float)
-        ##df['cross'] = np.where((df.Close.astype(float) >= df['BBU_50_2.0'].astype(float)) & (df.Close_1day_ago.astype(float) <= df['BBU_50_2_1day_ago'].astype(float)), "uband","na")
-        voldf = df[(df['Datetime'].dt.date >= datetime.strptime(current_day_dmy, '%d%m%Y').date() - timedelta(days=15)) & (df['Datetime'].dt.date <= datetime.strptime(current_day_dmy, '%d%m%Y').date())]
-        voldf['cross'] = np.where((voldf.Close.astype(float) <= voldf['BBL_50_2.0'].astype(float)), "lband",np.where((voldf.Close.astype(float) >= voldf['BBU_50_2.0'].astype(float)), "uband","na"))
-        allvols = list(set(voldf['cross'].tail(14).tolist()))
-        if('uband' in allvols): 
-            df['bb_crs'] = 'ub'
-        elif 'lband' in allvols:
-            df['bb_crs'] = 'lb'
-        else:
-            df['bb_crs'] = ''
-        df = df[df['Datetime'].astype(str).str.contains(str(datetime.strptime(current_day_dmy, '%d%m%Y').date().strftime("%Y-%m-%d")))]
-        df = df.tail(1)
-        mydf = pd.concat([mydf,df])
-        mydf = mydf.loc[:,['symbol','bbdiff1hr','bb_crs','BBU_1hr','BBL_1hr','hour_st','SMA_50','EMA_20','SMA_20_1hr']]
-        mydf = round(mydf, 2)
-        mydf.reset_index(inplace=True)
-    return mydf.loc[:,['symbol','bbdiff1hr','bb_crs','BBU_1hr','BBL_1hr','hour_st','SMA_50','EMA_20','SMA_20_1hr']]
+# def get1hourMC_bkp(fno,current_day_dmy):
+#     mydf = pd.DataFrame()
+#     for stock in fno:
+#         tday = datetime.strptime(current_day_dmy, '%d%m%Y').date() + timedelta(days=1)
+#         nse = Moneycontrol()
+#         df = nse.hist_data(stock,current_day_dmy,'60','3000')
+#         pd.set_option('display.max_columns', None)
+#         df['symbol'] = stock
+#         df.reset_index(inplace=True)
+#         df.set_index(pd.DatetimeIndex(df["Datetime"]).tz_localize(None), inplace=True)
+#         #df['d_adx'] = ta.adx(df['High'], df['Low'], df['Close'],length=14)['ADX_14']
+#         df.ta.ema(length=20, append=True)
+#         df.ta.sma(length=20, append=True)
+#         df.ta.bbands(close='Close', length=50, std=2, append=True) #BBL_50_2  BBM_50_2  BBU_50_2
+#         df.ta.supertrend(period=7, multiplier=3, append=True)
+#         df = df.rename(columns={"SUPERT_7_3.0":"hour_st"})
+#         df['bbdiff1hr'] = round(((df['BBU_50_2.0'].astype(float)-df['BBL_50_2.0'].astype(float))/df['BBU_50_2.0'].astype(float))*100,2)
+#         df['BBU_1hr'] = df['BBU_50_2.0'].astype(float)
+#         df['BBL_1hr'] = df['BBL_50_2.0'].astype(float)
+#         df['SMA_50'] = df['BBM_50_2.0'].astype(float)
+#         df['SMA_20_1hr'] = df['SMA_20'].astype(float)
+#         ##df['cross'] = np.where((df.Close.astype(float) >= df['BBU_50_2.0'].astype(float)) & (df.Close_1day_ago.astype(float) <= df['BBU_50_2_1day_ago'].astype(float)), "uband","na")
+#         voldf = df[(df['Datetime'].dt.date >= datetime.strptime(current_day_dmy, '%d%m%Y').date() - timedelta(days=15)) & (df['Datetime'].dt.date <= datetime.strptime(current_day_dmy, '%d%m%Y').date())]
+#         voldf['cross'] = np.where((voldf.Close.astype(float) <= voldf['BBL_50_2.0'].astype(float)), "lband",np.where((voldf.Close.astype(float) >= voldf['BBU_50_2.0'].astype(float)), "uband","na"))
+#         allvols = list(set(voldf['cross'].tail(14).tolist()))
+#         if('uband' in allvols): 
+#             df['bb_crs'] = 'ub'
+#         elif 'lband' in allvols:
+#             df['bb_crs'] = 'lb'
+#         else:
+#             df['bb_crs'] = ''
+#         df = df[df['Datetime'].astype(str).str.contains(str(datetime.strptime(current_day_dmy, '%d%m%Y').date().strftime("%Y-%m-%d")))]
+#         df = df.tail(1)
+#         mydf = pd.concat([mydf,df])
+#         mydf = mydf.loc[:,['symbol','bbdiff1hr','bb_crs','BBU_1hr','BBL_1hr','hour_st','SMA_50','EMA_20','SMA_20_1hr']]
+#         mydf = round(mydf, 2)
+#         mydf.reset_index(inplace=True)
+#     return mydf.loc[:,['symbol','bbdiff1hr','bb_crs','BBU_1hr','BBL_1hr','hour_st','SMA_50','EMA_20','SMA_20_1hr']]
 
 def get1hourMC(fno,current_day_dmy):
     df = pd.DataFrame()
@@ -214,3 +214,110 @@ def get1hourMC(fno,current_day_dmy):
             print("An exception occurred")
     return round(df[['symbol','bbdiff1hr','bb_crs','BBU_1hr','BBL_1hr','hour_st','SMA_50','EMA_20','SMA_20_1hr']],2)
 
+def get1dayMC(fno,current_day_dmy):
+    mydf = pd.DataFrame()
+    for stock in fno:
+        #tday = datetime.strptime(current_day_dmy, '%d%m%Y').date() + timedelta(days=1)
+        #startDay = tday - timedelta(days=130)
+        #duration = "1d"  # Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+        nse = Moneycontrol()
+        df = nse.hist_data(stock,current_day_dmy,'1D','300')
+        pd.set_option('display.max_columns', None)
+        df['symbol'] = stock
+        df.reset_index(inplace=True)
+        df.set_index(pd.DatetimeIndex(df["Datetime"]).tz_localize(None), inplace=True)
+        #df['d_adx'] = ta.adx(df['High'], df['Low'], df['Close'],length=14)['ADX_14']
+        df.ta.bbands(close='Close', length=50, std=2, append=True) #BBL_50_2  BBM_50_2  BBU_50_2
+        df.ta.supertrend(period=7, multiplier=3, append=True)
+        df = df.rename(columns={"SUPERT_7_3.0":"day_st"})
+        df['pp'] = (df.High.astype(float) + df.Low.astype(float) + df.Close.astype(float))/3
+        df['bc'] = round((df.High.astype(float) + df.Low.astype(float))/2,2)
+        df['tc'] = round((df.pp.astype(float) - df.bc.astype(float))+df.pp.astype(float),2)
+        df['cpr'] = round((abs(df.tc.astype(float)-df.bc.astype(float))/df.bc.astype(float))*100,2)
+        df['yest_cpr'] = df['cpr'].shift(1)
+        #df['cpr_churn'] = round(((df.yest_cpr.astype(float)-df.cpr.astype(float))/df.yest_cpr.astype(float))*100,2)
+        #df['bbandsdiff'] = round(((df['BBU_50_2.0'].astype(float)-df['BBL_50_2.0'].astype(float))/df['BBU_50_2.0'].astype(float))*100,2)
+        voldf = df[(df['Datetime'].dt.date >= datetime.strptime(current_day_dmy, '%d%m%Y').date() - timedelta(days=25)) & (df['Datetime'].dt.date < datetime.strptime(current_day_dmy, '%d%m%Y').date())]
+        voldff = voldf['Volume'].rolling(window=10).mean() #.iloc[-15:]
+        voldff4d = voldf['Volume'].rolling(window=4).mean() #.iloc[-15:]
+        voldf = voldf.join(voldff, rsuffix='_SMA')
+        voldf = voldf.join(voldff4d, rsuffix='_SMA4d')
+        voldf['smavol'] = np.where((voldf['Volume'].astype(float) > voldf['Volume_SMA'].astype(float)), "Y", np.nan) #smavol = list (voldf [ ' smavol ' ] . tail (5) . count () ) 
+        voldf['dsmavol'] = np.where((voldf['Volume'].astype(float) > voldf['Volume_SMA'].astype(float)) & (voldf['Close'].astype(float) >= voldf['Open'].astype(float)), "YY", np.nan)
+        #print (voldf) 
+        voldf = voldf.iloc[-5:]
+        smamorevol = len(list(voldf[voldf[ "dsmavol"]=='YY']["smavol"].to_list()))
+        smavol = len(list(voldf[voldf[ "smavol"]=='Y']["smavol"].to_list()))
+        df['Volume_SMA4d'] = voldf['Volume_SMA4d'].astype(float).iloc[-1]
+        df['Volume_2'] = df['Volume'].shift(1)
+        df['shock'] = round(df['Volume_2'].astype(float)/df['Volume_SMA4d'].astype(float),2)
+        df['todayshock'] = round(df['Volume'].astype(float)/df['Volume_SMA4d'].astype(float),2)
+        if(smamorevol>0): 
+            df['doubledayvol'] = str(smamorevol) +'gg'
+        else:
+            df['doubledayvol'] = ''
+        if(smavol>0): 
+            df['dayvol'] = str(smavol) +'D'
+        else:
+            df['dayvol'] = ''
+        df['BBU_1day'] = df['BBU_50_2.0'].astype(float)
+        df['BBL_1day'] = df['BBL_50_2.0'].astype(float)
+        df['dayvol'] = df['doubledayvol'] + df['dayvol']
+        df = df[df['Datetime'].astype(str).str.contains(str(datetime.strptime(current_day_dmy, '%d%m%Y').date().strftime("%Y-%m-%d")))]
+        df = df.head(1)
+        #print(df)
+#         df = df.tail(1)
+        mydf = pd.concat([mydf,df])
+        mydf = mydf.loc[:,['symbol','dayvol','BBU_1day','BBL_1day','day_st','shock','todayshock']]
+        mydf = round(mydf, 2)
+        mydf.reset_index(inplace=True)
+    return mydf.loc[:,['symbol','dayvol','BBU_1day','BBL_1day','day_st','shock','todayshock']]
+
+
+def getDayPivots(fno,current_day_dmy):
+    mydf = pd.DataFrame()
+    for stock in fno:
+        tday = datetime.strptime(current_day_dmy, '%d%m%Y').date() + timedelta(days=1)
+        startDay = tday - timedelta(days=100)
+        duration = "1mo"  # Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+        try:
+            proxyServer = urllib.request.getproxies()['http']
+        except KeyError:
+            proxyServer = ""
+        df = yf.download(
+            tickers=stock + ".NS",  start=startDay, end=tday,
+            #period=period,
+            interval=duration,
+            proxy=proxyServer,
+            progress=False,
+            timeout=10
+        )
+        pd.set_option('display.max_columns', None)
+        df['symbol'] = stock
+        df.reset_index(inplace=True)
+        df = df[df['Date'].dt.date<=tday]
+        ##full = df.tail(3).head(2)
+        # today = datetime.strptime(current_day_dmy, '%d%m%Y').date() - timedelta(days=1)
+        # bussiness_days_rng =pd.date_range(startDay, today, freq='BM')[-1].date()
+        # if (today ==  bussiness_days_rng):
+            # full = df.tail(3)
+        # else:
+            # full = df.tail(3).head(2)
+        full = df.tail(3).head(2)
+        #df = df[df['Date'].astype(str).str.contains(str(working_days[-1].strftime("%Y-%m-%d")))]
+        #full = df.head(1)
+        full['pp_day'] = (full.High.astype(float) + full.Low.astype(float) + full.Close.astype(float))/3
+        full['r1_day'] = round((2 * full.pp_day.astype(float)) - full.Low.astype(float),2)
+        full['s1_day'] = round((2 * full.pp_day.astype(float)) - full.High.astype(float),2)
+        full['r2_day'] = (full.pp_day.astype(float)) + (full.High.astype(float) - full.Low.astype(float))
+        full['s2_day'] = (full.pp_day.astype(float)) - (full.High.astype(float) - full.Low.astype(float))
+        
+        full['pp_day_prev'] = full['pp_day'].shift(1)
+        full['r1_day_prev'] = full['r1_day'].shift(1)
+        full['s1_day_prev'] = full['s1_day'].shift(1)
+        full['r2_day_prev'] = full['r2_day'].shift(1)
+        full['s2_day_prev'] = full['s2_day'].shift(1)
+        #print(full)
+        full = full.tail(1)
+        mydf = pd.concat([mydf,full])
+    return round(mydf,2).loc[:,['symbol','pp_day','r1_day','r2_day','s1_day','s2_day','pp_day_prev', 'r1_day_prev', 's1_day_prev', 'r2_day_prev', 's2_day_prev']]
