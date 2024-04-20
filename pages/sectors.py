cd2eab23c175341a93bb9fea1a35177c60c7112c
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-20 20:40:48 +0530

    Update sectors.py

--- pages/sectors.py
+++ pages/sectors.py
@@ -1,2 +1,19 @@
 import streamlit as st
 st.title("Sectors")
+
+import time
+
+button = st.button("start")
+placeholder = st.empty()
+
+if button:
+    with st.spinner():
+        counter = 0
+        while True:
+            print("Waiting...")
+            if placeholder.button("Stop", key=counter): # otherwise streamlit complains that you're creating two of the same widget
+                break
+            time.sleep(3)
+            counter += 1
+
+st.write("done")  # in this sample this code never executed
