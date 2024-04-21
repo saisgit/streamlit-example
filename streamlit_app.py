4428c99371f04d79bab069e1ee7d2311fb7e8422
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-21 19:36:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ def get_data():
     df = pd.DataFrame(data)
     return df
 
-st.autorefresh(interval=1 * 5 * 1000, key="")
+st.autorefresh(interval=1 * 5 * 1000)
 class NSE():
   pre_market_categories = ['NIFTY 50','Nifty Bank','Emerge','Securities in F&O','Others','All']
   equity_market_categories = []
