ac1ae061a9c1ee2a8d86b937e9f390ed176c9c2a
SaiKumar <29427150+saisgit@users.noreply.github.com>
2024-04-21 18:42:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -186,7 +186,6 @@ if button:
         counter = 0
         while True:
             print("Waiting...")
-            st.rerun()
             if placeholder.button("Stop", key=counter): # otherwise streamlit complains that you're creating two of the same widget
                 break
             time.sleep(15)
